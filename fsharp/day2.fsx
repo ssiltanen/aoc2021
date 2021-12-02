3d502790118977f60b373921ce5aff020699b96e
Sampo Siltanen <sampo.siltanen@reaktor.com>
2021-12-02 17:51:09 +0200

    Day 2

---
+++ fsharp/day2.fsx
@@ -0,0 +1,33 @@
+#time
+
+let input =
+    System.IO.File.ReadAllLines "data/day02.txt"
+
+let (|Up|Down|Forward|) (input: string) =
+    match input.Split(' ') with
+    | [| "up"; value |] -> Up(int value)
+    | [| "down"; value |] -> Down(int value)
+    | [| "forward"; value |] -> Forward(int value)
+    | _ -> failwithf "Unexpected input: %s" input
+
+input
+|> Array.fold
+    (fun (x, y) cmd ->
+        match cmd with
+        | Up value -> x, System.Math.Max(y - value, 0)
+        | Down value -> x, y + value
+        | Forward value -> x + value, y)
+    (0, 0)
+|> fun (x, y) -> x * y
+|> printfn "Day2-1 %i"
+
+input
+|> Array.fold
+    (fun (x, y, aim) cmd ->
+        match cmd with
+        | Up value -> x, y, System.Math.Max(aim - value, 0)
+        | Down value -> x, y, aim + value
+        | Forward value -> x + value, y + aim * value, aim)
+    (0, 0, 0)
+|> fun (x, y, _) -> x * y
+|> printfn "Day2-2 %i"
